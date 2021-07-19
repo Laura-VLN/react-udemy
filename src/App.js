1696b73f0784635fff5c76508c8e6709f2155aa6
EM-YA <70918970+EM-YA@users.noreply.github.com>
2021-07-19 16:47:59 +0200

    conditions - first part todo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import { useState } from 'react';
-import Item from './Components/Item/Item';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Form from './Components/Form'
 
 function App() {
 
-  const [inputData, setInputData] = useState(10);
-
-  const changeInput = (e) => {
-    // console.log(e);
-    setInputData(e);
-  }
-
-  console.log(inputData);
-
   return (
     <div className="App">
-      <h1>Hello depuis App !</h1>
-      <input type="text" value={inputData} onInput={e => changeInput(e.target.value)} />
+      <h1 className="text-center mt-3">To-Do List</h1>
+      <Form />
     </div>
   );
 }
---
+++ src/Components/Form.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import Item from './Item';
+
+export default function Form() {
+
+    const [dataArr, setDataArr] = useState([
+        {txt: "Promener le chien"},
+        {txt: "Sport"},
+        {txt: "Coder"}
+    ]);
+
+    return(
+        <div className="m-auto px-4 col-12 col-sm-10 col-lg-6">
+            <form className="mb-3">
+                <label htmlFor="todo" className="form-label mt-3">A faire</label>
+                <input type="text" className="form-control" id="todo"/>
+
+                <button className="mt-2 btn btn-primary d-block">Envoyer</button>
+            </form>
+            <h2>Liste des choses à faire : </h2>
+            <ul className="list-group">
+                {dataArr.map((item, index) => {
+                    return(
+                        <Item 
+                        txt={item.txt}
+                        key={index}
+                        />
+                    );
+                })}
+            </ul>
+        </div>
+    )
+
+}
